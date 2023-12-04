e331fa779c5c9bb98d6b00a6d6b6b32247f21738
Carlos Sandoval <carlos_72_75@hotmail.com>
2023-12-04 11:52:34 -0700

    Se agrego la formula de haversine en prolog

--- prolog/modulos/utils.pl
+++ prolog/modulos/utils.pl
@@ -7,3 +7,28 @@ row_to_list_dynamic(Row, List) :-
 
 term_to_list(Term, List) :-
     Term =.. [row | List].
+
+% Convert degrees to radians
+degrees_to_radians(Degrees, Radians) :-
+    Radians is Degrees * pi / 180.
+
+haversine_distance(Lat1, Lon1, Lat2, Lon2, Distance) :-
+    % Convert latitude and longitude from degrees to radians
+    degrees_to_radians(Lat1, RadLat1),
+    degrees_to_radians(Lon1, RadLon1),
+    degrees_to_radians(Lat2, RadLat2),
+    degrees_to_radians(Lon2, RadLon2),
+    
+    % Calculate differences
+    DeltaLat is RadLat2 - RadLat1,
+    DeltaLon is RadLon2 - RadLon1,
+    
+    % Haversine formula
+    A is sin(DeltaLat / 2) ** 2 + cos(RadLat1) * cos(RadLat2) * sin(DeltaLon / 2) ** 2,
+    C is 2 * atan2(sqrt(A), sqrt(1 - A)),
+    
+    % Radius of the Earth (in kilometers)
+    Radius is 6371.0,
+    
+    % Calculate distance
+    Distance is Radius * C.
